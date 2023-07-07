5d7d947164a5c805b7ef1ee502393dfe154a22d4
Mikhail <shaltynovm@gmail.com>
2023-07-07 19:17:05 +0530

    initial commit

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
+
+
     public void printGreatest(int first, int second, int third) {
+        int result = first > second ?
+                (second > third ? first : (third > first ? third : first))
+                : (second > third ? second : (third > first ? third : second));
+        System.out.println(result);
+
     }
 }
